66c9c869f8431f504d78b5aba6d5d2827577ec18
Rully Ikhsan <angledark68@gmail.com>
2019-06-17 14:20:32 +0700

    Update Generate-email.sql

--- Generate-email.sql
+++ Generate-email.sql
@@ -17,3 +17,27 @@ from fifapps.fs_mst_employees employee
 where employee.empl_status != 'RS'AND REGEXP_INSTR(empl_name,' ') = 0 AND email_address is null;
 
 ---------------------------------------------------------------------------------------------------
+
+
+
+--make function
+CREATE OR REPLACE Function GenerateEmail
+   RETURN varchar2 AS Email varchar2(50)
+
+BEGIN
+
+   SELECT employee.empl_name
+   FROM fifapps.fs_mst_employees employee
+
+   IF REGEXP_INSTR(empl_name,' ') != 0 and employee.empl_status != 'RS' and email_address is null THEN
+      Email := LOWER(REPLACE(CONCAT(REGEXP_SUBSTR(employee.EMPL_NAME , '[^ ]+' , 1 , 1),
+CONCAT('.', CONCAT(REGEXP_SUBSTR(employee.EMPL_NAME , '[^ ]+' , 1 , 2), '@csf.co.id' ))),' ',''));
+
+   ELSIF employee.empl_status != 'RS'and REGEXP_INSTR(empl_name,' ') = 0 and email_address is null
+      Email := LOWER(CONCAT(REPLACE(REGEXP_SUBSTR(employee.EMPL_NAME , '[^ ]+' , 1 , 1),' ',''), '@csf.co.id' ));
+
+   END IF;
+
+   RETURN Email;
+
+END Email;
